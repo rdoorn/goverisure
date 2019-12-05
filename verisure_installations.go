e84031ddf4f4318d62c049ad21835a0973a68466
Ronald Doorn <rdoorn@schubergphilis.com>
2019-12-05 16:54:09 +0100

    cleanup

--- verisure_installations.go
+++ verisure_installations.go
@@ -2,13 +2,6 @@ package goverisure
 
 import "encoding/json"
 
-/*
-{"data":{"count":1,"telemetries":[{"date":"2019-12-01 11:04:10","totalActivePower":695.0,"dcVoltage":748.125,"groundFaultResistance":7629.17,"powerLimit":100.0,"totalEnergy":10112.0,"temperature":35.0047,"inverterMode":"MPPT","operationMode":0,
-"vL1To2":395.531,"vL2To3":394.562,"vL3To1":398.688,"L1Data":{"acCurrent":1.13281,"acVoltage":230.219,"acFrequency":50.0101,"apparentPower":261.0,"activePower":234.0,"reactivePower":-115.0,"cosPhi":1.0},
-"L2Data":{"acCurrent":1.10938,"acVoltage":228.188,"acFrequency":50.0082,"apparentPower":260.0,"activePower":228.0,"reactivePower":-107.0,"cosPhi":1.0},
-"L3Data":{"acCurrent":1.13281,"acVoltage":228.141,"acFrequency":50.0098,"apparentPower":252.0,"activePower":233.0,"reactivePower":-115.0,"cosPhi":1.0}}]}}
-*/
-
 type Installation struct {
 	Giid            string `json:"giid"`
 	FirmwareVersion int    `json:"firmwareVersion"`
